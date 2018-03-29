cd2094ce9d93bffc98d081dada360ad01a20a39a
Steven Landers <steven.landers@gmail.com>
2018-03-29 10:38:01 -0400

    replacing with batch after nonce fix

--- vote-generate.js
+++ vote-generate.js
@@ -1,8 +1,9 @@
+const NUMBER_OF_VOTES = 20;
+const TARGET_ADDRESS = '0xa71cfeb932ec407304032600f91c796394465bb9';
+
 const Web3 = require("web3")
 const Accounts = require('web3-eth-accounts');
 const account = new Accounts('ws://localhost:8546');
-const fs = require('fs');
-
 const admin = require('firebase-admin');
 const serviceAccount = require('./.netvote.json');
 
@@ -10,8 +11,10 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
+let db = admin.firestore();
+let batch = db.batch();
+
 let web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
-const TARGET_ADDRESS = '0xa71cfeb932ec407304032600f91c796394465bb9';
 
 let votes = [
     "X8CdeKG0GxyYgP9qjlVOzdBDDucPw9fLsvATpmzTkfg=",
@@ -22,32 +25,33 @@ let votes = [
     "sHgTPVB06HBwGexxskXDMoZVtKsCRs7uI6FvAotVgs8="
 ];
 
-let db = admin.firestore();
-let batch = db.batch();
+const sendVotes = async(max) => {
+    for(let i =0; i<max; i++) {
+        let accts = [];
+        let acct = account.create();
+        accts.push(acct);
 
-for(let i =0; i<20; i++) {
-    let accts = [];
-    let acct = account.create();
-    accts.push(acct);
+        let voteId = web3.sha3(acct.address);
 
-    let voteId = web3.sha3(acct.address);
+        let votePayload = {
+            "address": TARGET_ADDRESS,
+            "encryptedVote": votes[Math.floor(Math.random() * votes.length)],
+            "passphrase": "generated",
+            "pushToken": "",
+            "timestamp": new Date().getTime(),
+            "tokenId": voteId,
+            "voteId": voteId
+        };
+        let voteRef = db.collection('transactionCastVote').doc(acct.address);
+        batch.set(voteRef, votePayload);
+    }
+    return batch.commit().then(function () {
+        console.log("sent "+max+" "+votes);
+    });
+};
 
-    let votePayload = {
-        "address": TARGET_ADDRESS,
-        "encryptedVote":  votes[Math.floor(Math.random()*votes.length)],
-        "passphrase": "generated",
-        "pushToken": "",
-        "timestamp": new Date().getTime(),
-        "tokenId": voteId,
-        "voteId": voteId
-    };
+sendVotes(NUMBER_OF_VOTES);
 
-    let voteRef = db.collection('transactionCastVote').doc(acct.address);
-    batch.set(voteRef, votePayload);
-}
 
-return batch.commit().then(function () {
-    console.log("done")
-});
 
 
